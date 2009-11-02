d20634d9567ef26ebe88883d80a4f1671e703120
Bryan Helmkamp <bryan@brynary.com>
2009-11-02 12:21:19 -0500

    Refactoring Middleware ND/BH

--- lib/asset_trip/middleware.rb
+++ lib/asset_trip/middleware.rb
@@ -12,28 +12,62 @@ module AssetTrip
     end
 
     def _call(env)
-      @path_info = Rack::Utils.unescape(env["PATH_INFO"])
+      @env = env
 
-      if @path_info.index(URL_PREFIX) == 0
-        return forbidden if @path_info.include?("..")
-
-        begin
-          @path = AssetTrip.config.js_load_path.resolve(@path_info[(URL_PREFIX.size + 1)..-1])
-        rescue UnknownAssetError
-          return not_found
-        end
-
-        if File.file?(@path) && File.readable?(@path)
-          serving
-        else
-          not_found
-        end
+      if matches_url_prefix?
+        process
       else
-        @app.call(env)
+        pass
       end
     end
 
-    def serving
+    def each
+      File.open(path, "rb") do |file|
+        while part = file.read(8192)
+          yield part
+        end
+      end
+    end
+
+  private
+
+    def matches_url_prefix?
+      path_info.index(URL_PREFIX) == 0
+    end
+
+    def process
+      return forbidden if path_info.include?("..")
+
+      if File.file?(path) && File.readable?(path)
+        serve_file
+      else
+        not_found
+      end
+    rescue UnknownAssetError
+      return not_found
+    end
+
+    def pass
+      @app.call(@env)
+    end
+
+    def path
+      @path ||= load_path.resolve(filename)
+    end
+
+    def filename
+      @filename ||= path_info[(URL_PREFIX.size + 1)..-1]
+    end
+
+    def path_info
+      @path_info ||= Rack::Utils.unescape(@env["PATH_INFO"])
+    end
+
+    def load_path
+      AssetTrip.config.js_load_path
+    end
+
+    def serve_file
       # NOTE:
       #   We check via File::size? whether this file provides size info
       #   via stat (e.g. /proc files often don't), otherwise we have to
@@ -51,14 +85,6 @@ module AssetTrip
         "Content-Type"   => Rack::Mime.mime_type(File.extname(@path), 'text/plain'),
         "Content-Length" => size.to_s
       }, body]
-    end
-
-    def each
-      File.open(@path, "rb") do |file|
-        while part = file.read(8192)
-          yield part
-        end
-      end
     end
 
     def forbidden
