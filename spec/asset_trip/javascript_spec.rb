04e1a61f002ab131168764fc83906a8ef7904679
Bryan Helmkamp <bryan@brynary.com>
2009-11-02 14:42:23 -0500

    Adding specs for Javascript

--- spec/asset_trip/javascript_spec.rb
+++ spec/asset_trip/javascript_spec.rb
@@ -2,7 +2,29 @@ require "spec_helper"
 
 describe AssetTrip::Javascript do
   describe "#contents" do
-    it "compresses the contents"
-    it "only runs the Compressor once for the package"
+    let :config do
+      stub(:paths => [Pathname.new("foo.css")])
+    end
+
+    let :compressor do
+      stub(:compress => "compressed")
+    end
+
+    before do
+      AssetTrip::Compressor.stub!(:new => compressor)
+      File.stub!(:read => "contents")
+    end
+
+    it "compresses the contents" do
+      AssetTrip::Compressor.should_receive(:new).with("js")
+      asset = AssetTrip::Javascript.new(config)
+      asset.contents.should == "compressed"
+    end
+
+    it "only runs the Compressor once for the package" do
+      compressor.should_receive(:compress).exactly(:once)
+      asset = AssetTrip::Javascript.new(config)
+      asset.contents.should == "compressed"
+    end
   end
 end
