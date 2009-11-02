ce819282252d109baed7eb90b09df166a21f7836
Bryan Helmkamp <bryan@brynary.com>
2009-11-02 15:24:30 -0500

    Use a Hash internally instead of inheriting all the behaviour

--- lib/asset_trip/manifest.rb
+++ lib/asset_trip/manifest.rb
@@ -1,15 +1,25 @@
 module AssetTrip
-  class Manifest < Hash
+  class Manifest
+
+    def initialize(md5sums = {})
+      @md5sums = md5sums
+    end
 
     def path_for(key)
       md5sum = self[key]
       "/" + File.join("assets", md5sum[0..1], md5sum[2..10], key)
     end
 
+    def []=(key, value)
+      @md5sums[key] = value
+    end
+
     def [](key)
-      md5sum = super
-      raise UnknownAssetError.new("Can't find #{key} in the manifest") unless md5sum
-      return md5sum
+      unless @md5sums.has_key?(key)
+        raise UnknownAssetError.new("Can't find #{key} in the manifest")
+      end
+
+      @md5sums[key]
     end
 
   end
--- spec/asset_trip/helper_spec.rb
+++ spec/asset_trip/helper_spec.rb
@@ -14,16 +14,16 @@ describe AssetTrip::Helper do
 
   describe "#javascript_include_asset" do
     it "generates a <script> tag based on the Manifest" do
-      AssetTrip.stub!(:manifest => { "foo.js" => "884695aafa07bf0c3e1f1fe578dd10d0"})
+      AssetTrip.stub!(:manifest => AssetTrip::Manifest.new("foo.js" => "884695aafa07bf0c3e1f1fe578dd10d0"))
       javascript_include_asset("foo").should be_like(<<-HTML)
         <script src="/assets/88/4695aafa0/foo.js" type="text/javascript"></script>
       HTML
     end
 
     it "generates multiple <script> tags" do
-      AssetTrip.stub!(:manifest => {
+      AssetTrip.stub!(:manifest => AssetTrip::Manifest.new(
         "foo.js" => "884695aafa07bf0c3e1f1fe578dd10d0",
-        "bar.js" => "1349f22e9bd188ef71503ba80624fc68" })
+        "bar.js" => "1349f22e9bd188ef71503ba80624fc68"))
       javascript_include_asset("foo", "bar").should be_like(<<-HTML)
         <script src="/assets/88/4695aafa0/foo.js" type="text/javascript"></script>
         <script src="/assets/13/49f22e9bd/bar.js" type="text/javascript"></script>
@@ -31,28 +31,28 @@ describe AssetTrip::Helper do
     end
 
     it "works with the file extension specified" do
-      AssetTrip.stub!(:manifest => { "foo.js" => "884695aafa07bf0c3e1f1fe578dd10d0"})
+      AssetTrip.stub!(:manifest => AssetTrip::Manifest.new("foo.js" => "884695aafa07bf0c3e1f1fe578dd10d0"))
       javascript_include_asset("foo.js").should be_like(<<-HTML)
         <script src="/assets/88/4695aafa0/foo.js" type="text/javascript"></script>
       HTML
     end
 
     it "works with symbols" do
-      AssetTrip.stub!(:manifest => { "foo.js" => "884695aafa07bf0c3e1f1fe578dd10d0"})
+      AssetTrip.stub!(:manifest => AssetTrip::Manifest.new("foo.js" => "884695aafa07bf0c3e1f1fe578dd10d0"))
       javascript_include_asset(:foo).should be_like(<<-HTML)
         <script src="/assets/88/4695aafa0/foo.js" type="text/javascript"></script>
       HTML
     end
 
     it "raises an UnknownAssetError if it's not in the manifest" do
-      AssetTrip.stub!(:manifest => {})
+      AssetTrip.stub!(:manifest => AssetTrip::Manifest.new)
       lambda {
         javascript_include_asset("foo")
       }.should raise_error(AssetTrip::UnknownAssetError)
     end
 
     it "does not add file mtimes into the query string" do
-      AssetTrip.stub!(:manifest => { "foo.js" => "884695aafa07bf0c3e1f1fe578dd10d0"})
+      AssetTrip.stub!(:manifest => AssetTrip::Manifest.new("foo.js" => "884695aafa07bf0c3e1f1fe578dd10d0"))
       stub!(:rails_asset_id => "rails_asset_id")
       javascript_include_asset("foo").should_not include("rails_asset_id")
     end
@@ -65,16 +65,16 @@ describe AssetTrip::Helper do
 
   describe "#stylesheet_link_asset" do
     it "generates a <style> tag based on the manifest" do
-      AssetTrip.stub!(:manifest => { "foo.css" => "884695aafa07bf0c3e1f1fe578dd10d0"})
+      AssetTrip.stub!(:manifest => AssetTrip::Manifest.new("foo.css" => "884695aafa07bf0c3e1f1fe578dd10d0"))
       stylesheet_link_asset("foo").should be_like(<<-HTML)
         <link href="/assets/88/4695aafa0/foo.css" media="screen" rel="stylesheet" type="text/css" />
       HTML
     end
 
     it "generates multiple <style> tags" do
-      AssetTrip.stub!(:manifest => {
+      AssetTrip.stub!(:manifest => AssetTrip::Manifest.new(
         "foo.css" => "884695aafa07bf0c3e1f1fe578dd10d0",
-        "bar.css" => "1349f22e9bd188ef71503ba80624fc68" })
+        "bar.css" => "1349f22e9bd188ef71503ba80624fc68"))
       stylesheet_link_asset("foo", "bar").should be_like(<<-HTML)
         <link href="/assets/88/4695aafa0/foo.css" media="screen" rel="stylesheet" type="text/css" />
         <link href="/assets/13/49f22e9bd/bar.css" media="screen" rel="stylesheet" type="text/css" />
@@ -82,35 +82,35 @@ describe AssetTrip::Helper do
     end
 
     it "supports passing options through to stylesheet_link_tag" do
-      AssetTrip.stub!(:manifest => { "foo.css" => "884695aafa07bf0c3e1f1fe578dd10d0"})
+      AssetTrip.stub!(:manifest => AssetTrip::Manifest.new("foo.css" => "884695aafa07bf0c3e1f1fe578dd10d0"))
       stylesheet_link_asset("foo", :media => "print").should be_like(<<-HTML)
         <link href="/assets/88/4695aafa0/foo.css" media="print" rel="stylesheet" type="text/css" />
       HTML
     end
 
     it "works with the file extension specified" do
-      AssetTrip.stub!(:manifest => { "foo.css" => "884695aafa07bf0c3e1f1fe578dd10d0"})
+      AssetTrip.stub!(:manifest => AssetTrip::Manifest.new("foo.css" => "884695aafa07bf0c3e1f1fe578dd10d0"))
       stylesheet_link_asset("foo.css").should be_like(<<-HTML)
         <link href="/assets/88/4695aafa0/foo.css" media="screen" rel="stylesheet" type="text/css" />
       HTML
     end
 
     it "works with symbols" do
-      AssetTrip.stub!(:manifest => { "foo.css" => "884695aafa07bf0c3e1f1fe578dd10d0"})
+      AssetTrip.stub!(:manifest => AssetTrip::Manifest.new("foo.css" => "884695aafa07bf0c3e1f1fe578dd10d0"))
       stylesheet_link_asset(:foo).should be_like(<<-HTML)
         <link href="/assets/88/4695aafa0/foo.css" media="screen" rel="stylesheet" type="text/css" />
       HTML
     end
 
     it "raises an UnknownAssetError if it's not in the manifest" do
-      AssetTrip.stub!(:manifest => {})
+      AssetTrip.stub!(:manifest => AssetTrip::Manifest.new)
       lambda {
         stylesheet_link_asset("foo")
       }.should raise_error(AssetTrip::UnknownAssetError)
     end
 
     it "does not add file mtimes into the query string" do
-      AssetTrip.stub!(:manifest => { "foo.css" => "884695aafa07bf0c3e1f1fe578dd10d0"})
+      AssetTrip.stub!(:manifest => AssetTrip::Manifest.new("foo.css" => "884695aafa07bf0c3e1f1fe578dd10d0"))
       stub!(:rails_asset_id => "rails_asset_id")
       stylesheet_link_asset("foo").should_not include("rails_asset_id")
     end
