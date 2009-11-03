ed091f4d96ddca1474a91c6a1d5603db68360a63
Bryan Helmkamp <bryan@brynary.com>
2009-11-03 11:59:23 -0500

    Add specs for ManifestWriter#write!

--- spec/asset_trip/manifest_writer_spec.rb
+++ spec/asset_trip/manifest_writer_spec.rb
@@ -2,10 +2,31 @@ require "spec_helper"
 
 describe AssetTrip::ManifestWriter do
   describe "#write!" do
-    it "creates the config/asset_trip directory if necessary"
-    it "writes a Ruby file"
-    it "initializes @manifest in AssetTrip to be a hash"
-    it "sets each asset in the hash"
-    it "stores the MD5 as the value in the hash"
+    it "writes a Ruby file" do
+      path = AssetTrip.app_root.join("config", "asset_trip", "manifest.rb")
+      AssetTrip::FileWriter.should_receive(:new).with(path).and_return(stub(:write! => nil))
+      manifest_writer = AssetTrip::ManifestWriter.new([])
+      manifest_writer.write!
+    end
+
+    it "initializes @manifest in AssetTrip to be a hash" do
+      file_writer = stub
+      file_writer.should_receive(:write!).with(<<-SRC)
+module AssetTrip
+  @manifest = Manifest.new
+end
+      SRC
+      AssetTrip::FileWriter.stub!(:new => file_writer)
+      manifest_writer = AssetTrip::ManifestWriter.new([])
+      manifest_writer.write!
+    end
+
+    it "stores the MD5 of each asset as the value in the hash" do
+      file_writer = stub
+      file_writer.should_receive(:write!).with(/@manifest\["foo.js"\] = "fa9bac2ae2d19ec2942a718253c55862"/)
+      AssetTrip::FileWriter.stub!(:new => file_writer)
+      manifest_writer = AssetTrip::ManifestWriter.new([stub(:asset, :name => "foo.js", :md5sum => "fa9bac2ae2d19ec2942a718253c55862")])
+      manifest_writer.write!
+    end
   end
 end
